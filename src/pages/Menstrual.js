8f1b73bcb684fdf5006504419f895504d3227599
Taylor Brooks <tybrooks01@icloud.com>
2024-02-27 13:46:38 -0500

    styling for menstural page complete

--- src/pages/Menstrual.js
+++ src/pages/Menstrual.js
@@ -1,9 +1,10 @@
+import "./Menstrual.scss";
 function Menstural() {
   return (
-    <div>
-      <h1>Menstural</h1>
-      <div>
-        <p>
+    <div class="menstrual-info">
+      <div class="menstrual-info__main-info">
+        <h1 class="menstrual-info__header">Menstrual</h1>
+        <p class="menstrual-info__paragraph-1">
           The menstrual phase marks the beginning of the menstrual cycle and
           occurs when the uterine lining, which was prepared for pregnancy
           during the luteal phase, is shed if no pregnancy occurs. This shedding
@@ -13,44 +14,35 @@ function Menstural() {
           days, with variations among individuals.
         </p>
       </div>
-      <div>
-        <p>
-          During the menstrual phase, the body may benefit from foods rich in
-          iron to replenish any loss due to menstruation. Including iron-rich
-          foods such as leafy greens, beans, and lean meats can help support
-          energy levels. Adequate hydration is crucial to compensate for fluid
-          loss during menstruation. Consuming foods high in vitamin C can
-          enhance iron absorption. Additionally, incorporating magnesium-rich
-          foods, like nuts and whole grains, may help alleviate menstrual
-          cramps.
-        </p>
-      </div>
-      <div>
-        <p>
-          Symptoms include: Menstrual cramps. Fatigue. Mood changes. Brains are
-          best at :
-        </p>
-      </div>
-      <div>
-        <p>
-          Gentle exercises such as walking or yoga can be beneficial during the
-          menstrual phase to promote circulation and alleviate discomfort. While
-          high-intensity workouts may be challenging, moderate-intensity
-          activities can provide a sense of well-being. Listening to your body's
-          signals and adjusting the intensity and duration of exercises
-          accordingly is key. Yoga poses that focus on gentle stretches and
-          relaxation can be particularly soothing.
-        </p>
-      </div>
-      <div>
-        <p>
-          During this period of your cycle, you might notice that your skin is
-          slightly more sensitive and a little more dehydrated. As your body is
-          shedding its uterine lining, your skin is actually producing less oil
-          so this is a great time to focus on hydration and caring for your skin
-          in its most sensitive state.
-        </p>
-      </div>
+      <p class="menstrual-info__paragraph-2">
+        During the menstrual phase, the body may benefit from foods rich in iron
+        to replenish any loss due to menstruation. Including iron-rich foods
+        such as leafy greens, beans, and lean meats can help support energy
+        levels. Adequate hydration is crucial to compensate for fluid loss
+        during menstruation. Consuming foods high in vitamin C can enhance iron
+        absorption. Additionally, incorporating magnesium-rich foods, like nuts
+        and whole grains, may help alleviate menstrual cramps.
+      </p>
+      <p class="menstrual-info__paragraph-3">
+        Symptoms include: Menstrual cramps. Fatigue. Mood changes. Brains are
+        best at :
+      </p>
+      <p class="menstrual-info__paragraph-4">
+        Gentle exercises such as walking or yoga can be beneficial during the
+        menstrual phase to promote circulation and alleviate discomfort. While
+        high-intensity workouts may be challenging, moderate-intensity
+        activities can provide a sense of well-being. Listening to your body's
+        signals and adjusting the intensity and duration of exercises
+        accordingly is key. Yoga poses that focus on gentle stretches and
+        relaxation can be particularly soothing.
+      </p>
+      <p class="menstrual-info__paragraph-5">
+        During this period of your cycle, you might notice that your skin is
+        slightly more sensitive and a little more dehydrated. As your body is
+        shedding its uterine lining, your skin is actually producing less oil so
+        this is a great time to focus on hydration and caring for your skin in
+        its most sensitive state.
+      </p>
     </div>
   );
 }
