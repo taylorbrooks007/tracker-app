fe82cdd7cbfa1cd76f8cd0a6526e8119a3cc7320
Taylor Brooks <tybrooks01@icloud.com>
2024-03-11 14:00:13 -0400

    luteal scss styling layout complete

--- src/pages/Luteal.js
+++ src/pages/Luteal.js
@@ -1,74 +1,71 @@
+import "./Luteal.scss";
 function Luteal() {
   return (
-    <div>
-      <h1>Luteal</h1>
-      <p>
-        The luteal phase is the second part of the menstrual cycle that happens
-        after ovulation and before your period starts. After an egg is released,
-        a structure called the corpus luteum forms and releases a hormone called
-        progesterone. Progesterone helps make the lining of the uterus thick and
-        ready for a possible pregnancy. If there's no pregnancy, the corpus
-        luteum breaks down, progesterone levels drop, and the uterine lining
-        sheds, causing your period. Some individuals may experience mood swings,
-        irritability, and fatigue as a result of the hormonal changes during
-        this time, contributing to the emotional and physical aspects of
-        premenstrual symptoms.
+    <div class="luteal-info">
+      <div class="luteal-info__main-info">
+        <h1 class="luteal-info__header">Luteal</h1>
+        <p class="luteal-info__paragraph-1">
+          The luteal phase is the second part of the menstrual cycle that
+          happens after ovulation and before your period starts. After an egg is
+          released, a structure called the corpus luteum forms and releases a
+          hormone called progesterone. Progesterone helps make the lining of the
+          uterus thick and ready for a possible pregnancy. If there's no
+          pregnancy, the corpus luteum breaks down, progesterone levels drop,
+          and the uterine lining sheds, causing your period. Some individuals
+          may experience mood swings, irritability, and fatigue as a result of
+          the hormonal changes during this time, contributing to the emotional
+          and physical aspects of premenstrual symptoms.
+        </p>
+      </div>
+      <p class="luteal-info__paragraph-2">
+        Your body is craving nutrient & calorie-dense foods to support your
+        cycle. Incorporating foods rich in complex carbohydrates, such as whole
+        grains, legumes, and vegetables, can help stabilize blood sugar levels
+        and combat potential mood swings or energy fluctuations. Including
+        sources of lean protein, like poultry, fish, or plant-based
+        alternatives, aids in muscle repair and supports satiety. Healthy fats
+        from sources like avocados, nuts, and olive oil contribute to hormone
+        synthesis and can assist in managing cravings.Cooked foods, can be more
+        beneficial for those who experience digestive discomfort or bloating
+        during the luteal phase. Cooking can break down certain fibers and make
+        nutrients more accessible. Incorporating steamed or lightly cooked
+        vegetables, whole grains, and lean proteins can be part of a
+        well-balanced and easily digestible diet. Additionally, prioritizing
+        foods high in vitamins B6 and magnesium, such as bananas, spinach, and
+        nuts, may help alleviate premenstrual symptoms. Staying hydrated and
+        listening to individual cravings while maintaining a balanced and varied
+        diet is key during this phase.
       </p>
-      <div>
-        <p>
-          Your body is craving nutrient & calorie-dense foods to support your
-          cycle. Incorporating foods rich in complex carbohydrates, such as
-          whole grains, legumes, and vegetables, can help stabilize blood sugar
-          levels and combat potential mood swings or energy fluctuations.
-          Including sources of lean protein, like poultry, fish, or plant-based
-          alternatives, aids in muscle repair and supports satiety. Healthy fats
-          from sources like avocados, nuts, and olive oil contribute to hormone
-          synthesis and can assist in managing cravings.Cooked foods, can be
-          more beneficial for those who experience digestive discomfort or
-          bloating during the luteal phase. Cooking can break down certain
-          fibers and make nutrients more accessible. Incorporating steamed or
-          lightly cooked vegetables, whole grains, and lean proteins can be part
-          of a well-balanced and easily digestible diet. Additionally,
-          prioritizing foods high in vitamins B6 and magnesium, such as bananas,
-          spinach, and nuts, may help alleviate premenstrual symptoms. Staying
-          hydrated and listening to individual cravings while maintaining a
-          balanced and varied diet is key during this phase.
-        </p>
-      </div>
-      <div>
-        <p>
-          Symptoms include: Mood changes. Tender breasts. Bloating. Fatigue.
-          Breaking out or acne. Changes in your appetite. Slight rise in body
-          temperature. Vaginal discharge. Brains are best at : Being detail
-          oriented Nesting & organizing Self-care
-        </p>
-      </div>
-      <div>
-        <p>
-          During the luteal phase of the menstrual cycle incorporating a mix of
-          exercises can be beneficial. Engaging in moderate-intensity aerobic
-          activities such as brisk walking, cycling, or swimming helps to
-          maintain cardiovascular health while minimizing stress on the body.
-          Additionally, incorporating mind-body practices like yoga or Pilates
-          may help alleviate stress and promote flexibility. Tailoring the
-          workout routine to individual energy levels and listening to the
-          body's signals is essential during this phase, allowing for a balanced
-          approach to fitness that aligns with the natural fluctuations in
-          hormonal levels.
-        </p>
-      </div>
-      <div>
-        <p>
-          At the beginning of the luteal phase, progesterone levels begin rising
-          which increases oil production & ultimately can lead to more
-          breakouts. Using products with BHA & niacinamide can assist with
-          reducing oil production. Make sure to clarify & remove any makeup or
-          excess products left on your skin as these can cause further
-          irritation. Towards the end of the luteal phase, estrogen levels drop
-          which can trigger eczema and skin sensitivities so using fragrance
-          free and sensitive-skin friendly products may be helpful at this time.
-        </p>
-      </div>
+
+      <p class="luteal-info__paragraph-3">
+        Symptoms include: Mood changes. Tender breasts. Bloating. Fatigue.
+        Breaking out or acne. Changes in your appetite. Slight rise in body
+        temperature. Vaginal discharge. Brains are best at : Being detail
+        oriented Nesting & organizing Self-care
+      </p>
+
+      <p class="luteal-info__paragraph-4">
+        During the luteal phase of the menstrual cycle incorporating a mix of
+        exercises can be beneficial. Engaging in moderate-intensity aerobic
+        activities such as brisk walking, cycling, or swimming helps to maintain
+        cardiovascular health while minimizing stress on the body. Additionally,
+        incorporating mind-body practices like yoga or Pilates may help
+        alleviate stress and promote flexibility. Tailoring the workout routine
+        to individual energy levels and listening to the body's signals is
+        essential during this phase, allowing for a balanced approach to fitness
+        that aligns with the natural fluctuations in hormonal levels.
+      </p>
+
+      <p class="luteal-info__paragraph-5">
+        At the beginning of the luteal phase, progesterone levels begin rising
+        which increases oil production & ultimately can lead to more breakouts.
+        Using products with BHA & niacinamide can assist with reducing oil
+        production. Make sure to clarify & remove any makeup or excess products
+        left on your skin as these can cause further irritation. Towards the end
+        of the luteal phase, estrogen levels drop which can trigger eczema and
+        skin sensitivities so using fragrance free and sensitive-skin friendly
+        products may be helpful at this time.
+      </p>
     </div>
   );
 }
